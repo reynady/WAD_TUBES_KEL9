aed6d7e13dee35e2c923837fb72e48d6b5182c96
jovanckabbigail <jovanckabbigail@student.telkomuniversity.ac.id>
2025-05-29 19:38:36 +0700

    update lagi

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengaduanController;
+use App\Http\Controllers\HomeController;
 
 Route::middleware(['auth'])->group(function(){
-    Route::get('/', [PengaduanController::class, 'index']);
+    Route::get('/', [PengaduanController::class, 'index'])->name('pengaduans.index');
     Route::resource('pengaduans', PengaduanController::class);
 });
 
-Auth::routes();
+
+Route::get('login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::post('login', function () {
+    $credentials = request(['email', 'password']);
+    if (auth()->attempt($credentials)) {
+        return redirect()->route('pengaduans.index');
+    }
+    return back()->withErrors(['email' => 'Invalid credentials']);
+});
+
+Route::post('logout', function () {
+    auth()->logout();
+    return redirect()->route('login');
+})->name('logout');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
